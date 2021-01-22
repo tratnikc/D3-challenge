03ee269b7b332b2bef59e0862bb137aaa0cfa3ea
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-22 16:26:14 -0500

    import data from csv

--- D3-data-journalism/assets/js/app.js
+++ D3-data-journalism/assets/js/app.js
@@ -25,3 +25,24 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
+// import the data from /data/data.csv
+d3.csv("assets/data/data.csv").then((hpdata) => {
+    console.log(hpdata);
+    // parse the data to numeric values
+    hpdata.forEach(function(data) {
+        // healthcare vs poverty
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        // smokers vs age
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+    });
+    console.log(hpdata);
+
+
+
+}).catch(function(error) {
+    console.log(error);
+});
+
+
