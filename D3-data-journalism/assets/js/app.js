fad428beeecd9aea16db610d89a4380238f19b54
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-23 18:43:03 -0500

    d3 tooltip

--- D3-data-journalism/assets/js/app.js
+++ D3-data-journalism/assets/js/app.js
@@ -1,30 +1,37 @@
 // set up chart
-var svgWidth = 960;
-var svgHeight = 500;
+var svgWidth = 960; // window.innerWidth;
+var svgHeight = 500;// window.innerHeight;
 
 var margin = {
     top: 20,
     right: 40,
-    bottom: 40,
-    left: 50
+    bottom: 60,
+    left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+ var w = parseInt(d3.select("#scatter").style("width"));
+ var h = parseInt(d3.select("#scatter").style("height"));
+
+ console.log(w);
+ console.log(h);
+
 // create an svg wrapper
 // append an SVG group that will hold the chart
 // and shift the group by left and top margins
 
-var svg = d3.select("#scatter")
+var svg = d3
+    .select("#scatter")
     .append("svg")
     .classed("chart", true)
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("width", svgWidth)  
+    .attr("height", svgHeight)
+
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 
 // import the data from /data/data.csv
 d3.csv("assets/data/data.csv").then((hpdata) => {
@@ -42,12 +49,12 @@ d3.csv("assets/data/data.csv").then((hpdata) => {
 
     // create scales for the chart
     var xScale = d3.scaleLinear()
-                    .range([0,width])
-                    .domain(d3.extent(hpdata, data => data.poverty));
+                    .domain([0, d3.max(hpdata, data => data.poverty)])
+                    .range([0,width]);
 
     var yScale = d3.scaleLinear()
-                    .range([height, 0])
-                    .domain([0, d3.max(hpdata, data => data.healthcare)]);
+                    .domain([0, d3.max(hpdata, data => data.healthcare)])
+                    .range([height, 0]);
 
     // create the axes
     var xAxis = d3.axisBottom(xScale);
@@ -56,29 +63,41 @@ d3.csv("assets/data/data.csv").then((hpdata) => {
     // append the axes
     // append y axis
     chartGroup.append("g")
-                .classed("axis", true)
+                .classed("y-axis", true)
                 .call(yAxis);
 
     // append x axis
     chartGroup.append("g")
-                .classed("axis", true)
+                .classed("x-axis", true)
                 .attr("transform", `translate(0, ${height})`)
                 .call(xAxis);
     
     // add the dots
-    var circlesGroup = chartGroup.append("g")
-                .classed("stateCircle", true)
-                .selectAll("circle")
+    var circlesGroup = chartGroup.selectAll(".dots")
+                // .selectAll("circle")
                 .data(hpdata)
                 .enter()
                 .append("circle")
+                .classed("stateCircle", true)
                 .attr("cx", d => xScale(d.poverty))
                 .attr("cy", d => yScale(d.healthcare))
-                .attr("r", 5);
+                .attr("r", 15);
 
     // append tooltip div
-    var toolTip = d3.select(".chart")
-                    .append("div");
+    var toolTip = d3.tip().attr('class', 'd3-tip').html(function(d) {
+        return `<div> ${d.state} <br> Poverty: ${d.poverty}% <br> HealthCare: ${d.healthcare}% </div>`; 
+    });
+
+    // toolTip
+    circlesGroup.call(toolTip);
+
+    // create mouseover
+    circlesGroup.on("mouseover", function (d) {
+                    toolTip.show(d, this);
+                })
+                .on("mouseout", function (d){ toolTip.hide(d)})
+
+
 
 
 
@@ -86,5 +105,3 @@ d3.csv("assets/data/data.csv").then((hpdata) => {
 }).catch(function(error) {
     console.log(error);
 });
-
-
